bc846215881d5d04d0e0c4b1702b3de6a8a4519a
Kristian Hennings <krhe@inventors.dk>
2023-12-29 13:29:51 +0100

    Work on jobs

--- Inventors.Xml/Configuration/Job.cs
+++ Inventors.Xml/Configuration/Job.cs
@@ -38,18 +38,28 @@ namespace Inventors.Xml.Configuration
 
         protected Type LoadType(string path, IJobConfiguration configuration)
         {
-            var assemblyPath = GetAssemblyPath(path, configuration);
-
-            if (!File.Exists(assemblyPath))
+            // Yes I know it is an ugly hack! But as our dear PM said: Live With It!
+            if (configuration.Assembly == "Inventors.Xml.dll")
             {
-                Console.WriteLine($"Did not find assembly: {assemblyPath}");
-                throw new ArgumentException("Assembly not found");
+                var assembly = typeof(XSDGConfig).Assembly;
+                var type = assembly.GetType(Type);
+                return type ?? throw new InvalidOperationException($"Failed to load type [ {Type} ]");
             }
+            else
+            {
+                var assemblyPath = GetAssemblyPath(path, configuration);
 
-            var assembly = Assembly.LoadFrom(assemblyPath);
-            var type = assembly.GetType(Type);
+                if (!File.Exists(assemblyPath))
+                {
+                    Console.WriteLine($"Did not find assembly: {assemblyPath}");
+                    throw new ArgumentException("Assembly not found");
+                }
 
-            return type ?? throw new InvalidOperationException($"Failed to load type [ {Type} ]");
+                var assembly = Assembly.LoadFrom(assemblyPath);
+                var type = assembly.GetType(Type);
+
+                return type ?? throw new InvalidOperationException($"Failed to load type [ {Type} ]");
+            }
         }
 
         public abstract void Run(string path, IJobConfiguration configuration);
