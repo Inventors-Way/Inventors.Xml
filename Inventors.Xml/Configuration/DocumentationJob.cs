3a38f036c27bde0146ebad53a9e939cd4fbfb733
Kristian Hennings <krhe@inventors.dk>
2023-12-29 16:02:35 +0100

    More concise format for jobs

--- Inventors.Xml/Configuration/DocumentationJob.cs
+++ Inventors.Xml/Configuration/DocumentationJob.cs
@@ -20,22 +20,15 @@ namespace Inventors.Xml.Configuration
 
         public override void Run(string path, IJobConfiguration configuration)
         {
-            Console.Write($"Loading type: {Type} ... ");
-            var type = LoadType(path, configuration);
-            Console.WriteLine("done");
-
-            Console.Write("Parsing type ... ");
-            var document = Inspector.Run(type);
-            Console.WriteLine("done");
-
-            Console.Write("Setting up documentation source ... ");
+            var type = $"Loading type: {Type} ... ".Evaluate(() => LoadType(path, configuration));
+            var document = "Parsing type ... ".Evaluate(() => Inspector.Run(type));
             var docPath = GetDocumentationPath(path, configuration);
-            var source = DocumentationSource.Create(document, docPath)
-                .SetInputFormat(DocumentationFileFormat)
-                .Build();
-            Console.WriteLine("done");
-            Console.WriteLine("Generating documentation files:");
-            var generator = new DocumentationGenerator(document, source);
+            var source = "Setting up documentation source ... ".Evaluate(() =>
+                DocumentationSource.Create(document, docPath)
+                    .SetInputFormat(DocumentationFileFormat)
+                    .Build());
+            var generator = "Creating documentation generator ...".Evaluate(() => new DocumentationGenerator(document, source));
+
             generator.Run();
         }
     }
---
+++ Inventors.Xml/Configuration/JobExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventors.Xml.Configuration
+{
+    public static class JobExtensions
+    {
+        public static T Evaluate<T>(this string str, Func<T> func)
+        {
+            T retValue = func();
+            Console.Write(str);
+            return retValue;
+        }
+    }
+}
