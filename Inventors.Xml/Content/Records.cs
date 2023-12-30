f81deb6b06f8d634da13b1a80e5801dc89d57d47
Kristian Hennings <krhe@inventors.dk>
2023-12-30 15:49:41 +0100

    More cleanup

---
+++ Inventors.Xml/Content/Records.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventors.Xml.Content
+{
+    public record AttributeDescriptor(string Name, string Type, bool Required, bool Primitive, string PropertyName);
+    public record ElementDescriptor(string Name, Element Type, bool Required, string PropertyName);
+    public record EnumValueDescriptor(string EnumValue, string Name);
+    public record ArrayItem(string Name, Element Type);
+    public record Choice(string Name, Element Type);
+
+
+}
