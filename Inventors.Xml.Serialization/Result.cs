50d398ef3245401783aa2e54984f832f7f42eea8
Kristian Hennings <krhe@inventors.dk>
2023-12-30 17:25:13 +0100

    Create Result.cs

---
+++ Inventors.Xml.Serialization/Result.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventors.Xml.Serialization
+{
+    public class Result<TValue, TError>
+        where TValue : class
+        where TError : class
+    {
+        private Result(TValue value)
+        {
+            _value = value;
+            _error = null;
+        }
+
+        private Result(TError error)
+        {
+            _value = null;
+            _error = error;
+        }
+
+        public static implicit operator Result<TValue, TError>(TValue value) => new Result<TValue, TError>(value);
+        public static implicit operator Result<TValue, TError>(TError error) => new Result<TValue, TError>(error);
+        public static implicit operator TValue(Result<TValue, TError> result) => result.Value;
+
+        public Result<TValue, TError> OnSuccess(Action<TValue> action)
+        {
+            if (_value is null)
+                return this;
+
+            action(_value);
+            return this;
+        }
+
+        public Result<TValue, TError> OnError(Action<TError> action)
+        {
+            if (_error is null)
+                return this;
+
+            action(_error);
+            return this;
+        }
+
+        public TValue Value
+        {
+            get
+            {
+                if (_value is null)
+                    throw new InvalidOperationException("Operation failed");
+
+                return _value;
+            }
+        }
+
+        private readonly TValue? _value;
+        private readonly TError? _error;
+    }
+}
