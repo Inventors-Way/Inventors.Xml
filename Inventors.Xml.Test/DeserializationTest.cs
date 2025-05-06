b7731d14e25ff37244a654d79bd4ccf331124d4c
Kristian Hennings <krhe@inventors.dk>
2025-05-06 09:27:17 +0200

    Test of deserialization

---
+++ Inventors.Xml.Test/DeserializationTest.cs
@@ -0,0 +1,24 @@
+﻿using Inventors.Xml.Serialization;
+using Inventors.Xml.Test.TestObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventors.Xml.Test
+{
+    [TestClass]
+    public class DeserializationTest
+    {
+        [TestMethod]
+        public void T01_DeserializeObjectHirachy()
+        {
+            var text = File.ReadAllText(Path.Combine(IntegrationTests.DataDirectory, "final-object.xml"));
+            var data = text.ToObject<FinalClass>();
+
+            Console.WriteLine(data);
+
+        }
+    }
+}
---
+++ Inventors.Xml.Test/TestObjects/FinalClass.cs
@@ -0,0 +1,90 @@
+﻿using Inventors.Xml.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Inventors.Xml.Test.TestObjects
+{
+    public class A
+    {
+        [XmlAttribute("a-factor")]
+        public int Afactor { get; set; }
+
+        public override string ToString() => $"A {Afactor}";
+    }
+
+    public class  B
+    {
+        [XmlAttribute("b-factor")]
+        public int Bfactor { get; set; }
+
+        public override string ToString() => $"B {Bfactor}";
+    }
+
+    public class C
+    {
+        [XmlAttribute("c-factor")]
+        public int Cfactor { get; set; }
+
+        public override string ToString() => $"C {Cfactor}";
+    }
+
+    public class D
+    {
+        [XmlAttribute("d-factor")]
+        public int Dfactor { get; set; }
+
+        public override string ToString() => $"D {Dfactor}";
+    }
+
+    public class E
+    {
+        [XmlAttribute("e-factor")]
+        public int Efactor { get; set; }
+
+        public override string ToString() => $"E {Efactor}";
+    }
+
+    public class F
+    {
+        [XmlAttribute("f-factor")]
+        public int Ffactor { get; set; }
+
+        public override string ToString() => $"F {Ffactor}";
+    }
+
+    [XmlRoot("final-class")]
+    public class FinalClass : BaseClassB
+    {
+        [XmlElement("d")]
+        public D? D { get; set; } 
+
+        [XmlElement("f")]
+        public F? F { get; set; } 
+    }
+
+    public abstract class BaseClassB : BaseClassA
+    {
+        [XmlAttribute("value")]
+        public int Value { get; set; }
+
+        [XmlElement("b")]
+        public B? B { get; set; } 
+
+        [XmlElement("c")]
+        public C? C { get; set; } 
+    }
+
+    public abstract class BaseClassA
+    {
+        [XmlAttribute("name")]
+        [XmlRequired]
+        public string Name { get; set; } = string.Empty;
+
+        [XmlElement("a")]
+        public A? A { get; set; } 
+    }
+}
